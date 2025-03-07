45ea664026fb68089a20c38976a35f5d0fe22f86
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-07 13:10:12 +0000

    バリデーション

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :user_id, uniqueness: {scope: :book_id}
+  #バリデーションとテーブルのUNIQUE制約
+  #user_idについて、book_idとセットのすでに同じ値のペアがないか判定
 end

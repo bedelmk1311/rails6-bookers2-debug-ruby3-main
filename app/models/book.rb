17c139ea448dab2b82f9446c829b5060074621e1
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-07 13:06:34 +0000

    モデル関連付け

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,15 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  #関連付け
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  #exists存在する favorited_by?メソッド作成
+  #(引数で渡されたユーザid)がFavoritesテーブル内に存在するかどうか
 end

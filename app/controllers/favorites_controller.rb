b294bae85ba12df78194fd356144f4c8f1dd1e9f
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-07 13:28:16 +0000

    作成削除機能追加

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,20 @@
+class FavoritesController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorite.new(book_id: book.id)
+     #favorite = Favorite.new(favorite_params)
+     #いいね作成
+     #favorite.user_id = current_user.id と同義？
+    favorite.save
+    redirect_to book_path(book_image)
+  end
+    
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    #find_byメソッド　条件を指定して最初の1件を取得（該当する一個だけ）
+    favorite.destroy
+    redirect_to book_path(book_image)
+  end
+
+end

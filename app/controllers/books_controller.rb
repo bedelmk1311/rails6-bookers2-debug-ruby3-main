49c9fe930f9ba392c73b71c88a5cdd30b104ad75
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-07 05:07:13 +0000

    [7]解説デバイス

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :ensure_correct_user #, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])

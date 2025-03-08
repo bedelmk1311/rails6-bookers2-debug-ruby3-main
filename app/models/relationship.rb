87a03c560bbc9ad9d124210a3cc773467dd390a5
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-08 06:58:34 +0000

    Userモデル_検索

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,8 +1,10 @@
 class Relationship < ApplicationRecord
-  belongs_to :follower, class_name: "User"
-  belongs_to :followed, class_name: "User"
   #クラス名（モデル名）を指定しないと
   #followersテーブルを参照しにいってしまうためエラーに
+  belongs_to :follower, class_name: "User"
+  #Userモデル内、フォローされる側と関連
+  belongs_to :followed, class_name: "User"
+  #Userモデル内、フォローする側と関連
 
   #１人のユーザーが１人に対して重複してフォローできないようバリデーション
   #validates :followed_id, uniqueness: {scope: :follower_id}？
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,32 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
-  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'follower_id' dependent: :destroy
-  has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'followed_id' dependent: :destroy
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'follower_id' ,dependent: :destroy
   #ユーザーはたくさんフォローした人のIDを持てる　ユーザー削除で消える
+  has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'followed_id' ,dependent: :destroy
   #ユーザーはフォローされた人のIDをもてる　ユーザー削除で消える
 
-  has_many :follower_relationships, through: :relationship, source: :follower
-  #relationshipを通してfollowerを取得する？
-  has_many :followed_relationships, through: :relationship, source: :followed
-  #relationshipを通してfollowedを取得する？
+  has_many :followings, through: :follower_relationship, source: :follower
+  #follower_relationshipを通してfollowerを取得する =フォローしているユーザーを取得
+  has_many :followed_relationships, through: :followed_relationship, source: :followed
+  #followeed_relationshipを通してfollowedを取得する =フォロワーを取得
+
+  #指定したユーザーをフォローするメソッド
+  def follow(user)
+    follower_relationships.create(followed_id: user.id)
+  end
   
-  #仮の命名した上で中間テーブルのre~テーブルを通してfo~テーブルからデータを取得できるように
+  #指定したユーザーのフォローを解除するメソッド
+  def unfollow(user)
+    follower_relationships.find_by(followed_id: user.id).destroy
+    #find_byメソッドで該当する引数を探す
+  end
+  
+  # 指定したユーザーをフォローしているかどうかを？で判定
+  def following?(user)
+    followings.include?(user)
+  end
+
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}

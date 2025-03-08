74aa027b5df13a0171aa2d9ea82853f3a5afdef1
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-08 07:22:17 +0000

    アクションの追加_検索

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,31 @@
 class RelationshipsController < ApplicationController
+  def create
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+    #Userモデルで定義したfollowメソッド発動
+    redirect_to request.referer
+    #遷移元のURL情報を取得 =同じ画面に戻る
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+    #Userモデルで定義したunfollowメソッド発動
+    redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    #対象のuserを特定
+    @users = user.followings
+    #フォローしているユーザーを取得(Userモデルで定義)
+  end
+  
+  def followers
+    user = User.find(params[:user_id])
+    #対象のuserを特定
+    @users = user.followers
+    #フォロワーを取得(Userモデルで定義)
+  end
+
 end

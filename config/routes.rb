f67c525208946372687984b55465d74e5dbde1a8
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-08 15:59:11 +0000

    viewページ用ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,8 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-      #get "followings" => "relationships#followings", as: "followings"
-  	  #get "followers" => "relationships#followers", as: "followers"
-      #viewページ用？
+      get "followings" => "relationships#followings", as: "followings"
+  	  get "followers" => "relationships#followers", as: "followers"
+      #viewページ用のルーティング
   end
 end

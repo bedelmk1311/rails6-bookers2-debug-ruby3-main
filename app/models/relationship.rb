d1a2c2acad71d1ee1390d45db084eec49641667b
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-08 04:37:52 +0000

    重複フォローバリデーション仮

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,9 @@
 class Relationship < ApplicationRecord
+  belongs_to :follower, class_name: "User"
+  #クラス名（モデル名）を指定しないと
+  #followersテーブルを参照しにいってしまうためエラーに
+
+  #１人のユーザーが１人に対して重複してフォローできないようバリデーション
+  #validates :followed_id, uniqueness: {scope: :follower_id}？
+  #
 end

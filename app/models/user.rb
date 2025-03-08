57026219b2174b6dbdaf046a10b2f584777c714e
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-08 05:20:55 +0000

    仮リレーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,18 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'follower_id' dependent: :destroy
+  has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'followed_id' dependent: :destroy
+  #ユーザーはたくさんフォローした人のIDを持てる　ユーザー削除で消える
+  #ユーザーはフォローされた人のIDをもてる　ユーザー削除で消える
+
+  has_many :follower_relationships, through: :relationship, source: :follower
+  #relationshipを通してfollowerを取得する？
+  has_many :followed_relationships, through: :relationship, source: :followed
+  #relationshipを通してfollowedを取得する？
+  
+  #仮の命名した上で中間テーブルのre~テーブルを通してfo~テーブルからデータを取得できるように
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 

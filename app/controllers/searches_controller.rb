5ba9de0314a5832b7f3550732f433975512a6478
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-09 08:30:07 +0000

    searchアクションの追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,22 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  #ユーザー認証　=アクションする前にログインしているかどうかを確認
+  
+  def search
+    @model = params[:model]
+    #リクエストから送られてきた値（ここではUserかBookモデル）がなんなのかを
+    @keyword = params[:keyword]
+    #ユーザーが入れた検索語句という意味でkeyword 
+    @method = params[:method]
+    #ユーザーが選択した４つの検索方法(search method)
+
+
+    # 選択したモデルで条件分岐
+    if @model  == "user"
+      @results = User.search_for(@keyword, @method)
+    else
+      @results = Books.search_for(@keyword, @method)
+    end
+    #search_for メソッド
+  end
 end

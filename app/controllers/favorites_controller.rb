fcf01e0d29df5acdcff0654853b31238c11924e0
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-03-10 11:12:31 +0000

    解説の続き

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,10 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = book.favorites.new(user_id: current_user.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
      #favorite = Favorite.new(favorite_params)
-     #現在のユーザーのいいねを作成（そのIDはuser.id）
+     #現在のユーザーのいいねを作成
      #favorite.user_id = current_user.id と同義？
     favorite.save
     #redirect_to request.referer
@@ -13,8 +13,8 @@ class FavoritesController < ApplicationController
   end
     
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = book.favorites.find_by(user_id: current_user.id)
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
     #find_byメソッド　条件を指定して最初の1件を取得（該当する一個だけ）
     favorite.destroy
     #redirect_to request.referer
